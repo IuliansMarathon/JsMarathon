3cc5bbd5a358321b30d5c2dfed2b1afe3fbda0a7
IulianBondarciuk <preascai@mail.ru>
2021-10-13 15:58:20 +0300

    homework-2

---
+++ MK/main.js
@@ -0,0 +1,54 @@
+// function createFullName(name, secondName, lastName) {
+//     const result = name + ' ' + secondName + ' ' + lastName;
+//     return result;
+// }
+
+// const fullName = createFullName('Zar', 'Zakharov', 'Konstantinov');
+// console.log(fullName);
+
+// const player1 = {
+//     name: 'Scorpion',
+//     hp: 100,
+//     nameName: true,
+//     name_name: true,
+//     100: true,
+//     $: true,
+//     _: true,
+//     attack: function() {
+//         console.log('Fight...');
+//     }
+// };
+
+// player1.attack();
+
+// console.log(player1);
+// console.log(player1.name);
+// console.log(player1['hp']);
+
+// const count = 100;
+// console.log(player1[count]);
+
+// player1.weapon = 'kunai';
+// console.log(player1);
+
+// player1.hp = player1.hp - 20;
+// console.log(player1.hp);
+
+// delete player1.hp;
+// delete player1.nameName;
+
+// console.log(player1);
+
+
+// const message = ['Hello', true, 2021];
+
+// console.log(message);
+
+// console.log(message[0]);
+
+// message.push('World');
+// console.log(message);
+
+// const str = message[0] + ' ' + message[3];
+// console.log(str);
+
