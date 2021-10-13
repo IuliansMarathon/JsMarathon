65c4b7a550bcb75c2c03d384c73b1131f216f658
IulianBondarciuk <preascai@mail.ru>
2021-10-13 17:08:46 +0300

    task 3

--- MK/main.js
+++ MK/main.js
@@ -1,54 +1,52 @@
-// function createFullName(name, secondName, lastName) {
-//     const result = name + ' ' + secondName + ' ' + lastName;
-//     return result;
-// }
 
-// const fullName = createFullName('Zar', 'Zakharov', 'Konstantinov');
-// console.log(fullName);
+const player1 = {
+    name: 'Scorpion',
+    hp: 25,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon: [ 
+        'ak-47',
+    ],
+    attack: function() {
+        console.log(this.name + ' - Fight...');
+    }
+}
 
-// const player1 = {
-//     name: 'Scorpion',
-//     hp: 100,
-//     nameName: true,
-//     name_name: true,
-//     100: true,
-//     $: true,
-//     _: true,
-//     attack: function() {
-//         console.log('Fight...');
-//     }
-// };
+const player2 = {
+    name: 'SubZero',
+    hp: 75,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+    weapon: [ 
+        'm4a1-s'
+    ],
+    attack: function() {
+        console.log(this.name + ' - Fight...');
+    }
+}
 
-// player1.attack();
+function createPlayer (playerClass, playerName) {
+    const $player = document.createElement('div');
+    const $progressbar = document.createElement('div')
+    const $character = document.createElement('div');
+    const $life = document.createElement('div');
+    const $name = document.createElement('div');
+    const $img = document.createElement('img');
+    const $arenas = document.querySelector('.arenas');
 
-// console.log(player1);
-// console.log(player1.name);
-// console.log(player1['hp']);
+    $img.src = playerName.img;
+    $player.classList.add(playerClass);
+    $progressbar.classList.add('progressbar');
+    $character.classList.add('character');
+    $life.classList.add('life');
+    $name.classList.add('name');
+    $arenas.appendChild($player);
+    $player.appendChild($progressbar);
+    $player.appendChild($character);
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+    $character.appendChild($img);
+    $life.style.width = `${playerName.hp}%`;
+    $name.innerText = playerName.name;
+}
 
-// const count = 100;
-// console.log(player1[count]);
-
-// player1.weapon = 'kunai';
-// console.log(player1);
-
-// player1.hp = player1.hp - 20;
-// console.log(player1.hp);
-
-// delete player1.hp;
-// delete player1.nameName;
-
-// console.log(player1);
-
-
-// const message = ['Hello', true, 2021];
-
-// console.log(message);
-
-// console.log(message[0]);
-
-// message.push('World');
-// console.log(message);
-
-// const str = message[0] + ' ' + message[3];
-// console.log(str);
-
+createPlayer('player1', player1);
+createPlayer('player2', player2);
