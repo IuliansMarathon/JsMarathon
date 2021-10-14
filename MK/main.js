b4226018f24616f13083c269d285db749bc34bcb
IulianBondarciuk <preascai@mail.ru>
2021-10-14 16:20:34 +0300

    Last

--- MK/main.js
+++ MK/main.js
@@ -1,117 +1,35 @@
-//! TASK 0
-
 const player1 = {
-    name: 'Scorpion',
-    hp: 25,
+    name: 'SCORPION',
+    hp: 80,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-    weapon: [ 
-        'ak-47',
+    weapon: [
+        'ak-47'
     ],
     attack: function() {
-        console.log(this.name + ' - Fight...');
+        console.log(this.name + ' Fight...');
     }
 }
 
 const player2 = {
-    name: 'SubZero',
-    hp: 75,
-    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
-    weapon: [ 
-        'm4a1-s'
+    name: 'SCONYA',
+    hp: 90,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
+    weapon: [
+        'ak-47'
     ],
     attack: function() {
-        console.log(this.name + ' - Fight...');
+        console.log(this.name + ' Fight...');
     }
 }
 
-//! TASK 1 
-
-function createPlayer(player, name) {
-
-    const $arenas = document.querySelector('.arenas');
-    
-    const $player1 = document.createElement('div');
-    $player1.classList.add(player);
-
-    const $progressbar = document.createElement('div');
-    $progressbar.classList.add('progressbar');
-
-    const $character = document.createElement('div');
-    $character.classList.add('character');
-
-    const $life = document.createElement('div');
-    $life.classList.add('life');
-    $life.style.width = `100%`;
-
-    const $name = document.createElement('div');
-    $name.classList.add('name');
-    $name.innerText = name;
-
-    const $img = document.createElement('img');
-    $img.classList.add('img');
-    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
-
-    $arenas.appendChild($player1);
-    $player1.appendChild($progressbar);
-    $player1.appendChild($character);
-    $progressbar.appendChild($life);
-    $progressbar.appendChild($name);
-    $character.appendChild($img);
-}
-
-createPlayer('player1', 'SCORPION');
-
-//! TASK 2
-
-function createPlayer(player, name, hp) {
-
-    const $arenas = document.querySelector('.arenas');
-    
-    const $player1 = document.createElement('div');
-    $player1.classList.add(player);
-
-    const $progressbar = document.createElement('div');
-    $progressbar.classList.add('progressbar');
-
-    const $character = document.createElement('div');
-    $character.classList.add('character');
-
-    const $life = document.createElement('div');
-    $life.classList.add('life');
-    $life.style.width = `100%`;
-    $life.innerText = hp;
-
-    const $name = document.createElement('div');
-    $name.classList.add('name');
-    $name.innerText = name;
-
-    const $img = document.createElement('img');
-    $img.classList.add('img');
-    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif';
-    $img.alt = name;
-
-    $arenas.appendChild($player1);
-    $player1.appendChild($progressbar);
-    $player1.appendChild($character);
-    $progressbar.appendChild($life);
-    $progressbar.appendChild($name);
-    $character.appendChild($img);
-}
-
-createPlayer('player1', 'SCORPION', 50);
-createPlayer('player2', 'SUB-ZERO', 80);
-
-
-//! TASK 3(*)
-
-function createPlayer (playerClass, playerName) {
+function createPlayer(playerClass, playerName) {
 
     const $arenas = document.querySelector('.arenas');
 
     const $player = document.createElement('div');
     $player.classList.add(playerClass);
 
-    const $progressbar = document.createElement('div')
+    const $progressbar = document.createElement('div');
     $progressbar.classList.add('progressbar');
 
     const $character = document.createElement('div');
@@ -119,6 +37,7 @@ function createPlayer (playerClass, playerName) {
 
     const $life = document.createElement('div');
     $life.classList.add('life');
+    $life.innerText = playerName.hp;
     $life.style.width = `${playerName.hp}%`;
 
     const $name = document.createElement('div');
@@ -127,15 +46,16 @@ function createPlayer (playerClass, playerName) {
 
     const $img = document.createElement('img');
     $img.src = playerName.img;
-    
-    
+    $img.alt = playerName.name;
+
     $arenas.appendChild($player);
     $player.appendChild($progressbar);
     $player.appendChild($character);
     $progressbar.appendChild($life);
     $progressbar.appendChild($name);
     $character.appendChild($img);
+
 }
 
-createPlayer('player1', player1);
-createPlayer('player2', player2);
+createPlayer('player1', player1)
+createPlayer('player2', player2)
